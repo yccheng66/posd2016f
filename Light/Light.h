b879331a23d1a581eb0ff020a37071d6391db90d
yccheng66 <yccheng66@gmail.com>
2016-11-16 17:04:41 +0800

    Light with states: first completion

--- Light/Light.h
+++ Light/Light.h
@@ -1,20 +1,36 @@
 #ifndef LIGHT_H_INCLUDED
 #define LIGHT_H_INCLUDED
 
+
 class LightState{
     public:
-        virtual void flip() {}
-        std::string getLightState()const {return state;}
-    protected:
-        LightState(std::string s="off"):state(s){}
-        std::string state;
+        virtual LightState * flip() = 0;
+        virtual std::string getLightState() const = 0;
+        virtual ~LightState() {}
 };
 class OffState : public LightState{
     public:
         OffState(){}
-        void flip(){
-            state = "med";
-        }
+        LightState * flip();
+        std::string getLightState() const {return "off";}
+        virtual ~OffState() {}
+};
+
+class MedState : public LightState{
+    public:
+        MedState(){}
+        LightState * flip();
+        std::string getLightState() const {return "med";}
+        virtual ~MedState() {}
+};
+
+class HighState : public LightState{
+    public:
+        HighState(){}
+        LightState * flip();
+        std::string getLightState() const {return "high";}
+        virtual ~HighState() {}
+
 };
 
 class Light{
@@ -26,13 +42,25 @@ public:
     }
     void flip()
     {
-        state->flip();
+        LightState* ls = state;
+        state = state->flip();
+        delete ls;
     }
 
 private:
     LightState* state;
 };
 
+LightState * OffState :: flip(){
+    return new MedState;
+}
 
+LightState * MedState :: flip(){
+    return new HighState;
+}
+
+LightState * HighState :: flip(){
+    return new OffState;
+}
 
 #endif // LIGHT_H_INCLUDED
--- Light/utLight.h
+++ Light/utLight.h
@@ -13,18 +13,20 @@ TEST(flip,Light)
     light.flip();
     CHECK(std::string("med")==light.getState());
 }
-/*
+
+
 TEST(medToHigh,Light)
 {
-    Light light("med");
+    Light light(new MedState);
     light.flip();
     CHECK(std::string("high")==light.getState());
 }
+
 TEST(hightoOff,Light)
 {
-    Light light("high");
+    Light light(new HighState);
     light.flip();
     CHECK(std::string("off")==light.getState());
-}*/
+}
 
 #endif // UTLIGHT_H_INCLUDED
